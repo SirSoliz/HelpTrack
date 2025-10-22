f0618625e7acfda0b2fedc3d3ac3c064e2154c5b
Thannia <thanniaporras19@gmail.com>
2025-10-21 19:04:55 -0600

    Se agregan lineas para la conexion de base de datos según el servidor y se instalan paquetes NuGet

---
+++ HelpTrack.Application/DTOs/TecnicoDTO.cs
@@ -0,0 +1,22 @@
+﻿using HelpTrack.Infraestructure.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HelpTrack.Application.DTOs
+{
+    public record TecnicoDTO
+    {
+        [Display(Name = "Id Tecnico")]
+        [ValidateNever]
+        public int IdTecnico { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "{0} es un dato requerido")]
+        public string Nombre { get; set; } = null!;
+        //public virtual List<Tecnico> Tecnico { get; set; } = new List<Tecnico>();
+    }
+}
