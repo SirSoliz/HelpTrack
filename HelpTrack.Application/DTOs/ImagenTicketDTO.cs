7459c1e5aea2d05f4c81fcd6d66686ac4d0819a4
SirSoliz <bsolism5@hotmail.com>
2025-11-04 15:20:53 -0600

    ImagenDTO

--- HelpTrack.Application/DTOs/ImagenTicketDTO.cs
+++ HelpTrack.Application/DTOs/ImagenTicketDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelpTrack.Infraestructure.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace HelpTrack.Application.DTOs
 {
-    internal class ImagenTicketDTO
+    public record ImagenTicketDTO
     {
+        public long IdImagen { get; set; }
+
+        public int IdTicket { get; set; }
+
+        public int IdUsuario { get; set; }
+
+        public string NombreArchivo { get; set; } = null!;
+
+        public string TipoContenido { get; set; } = null!;
+
+        public string UrlArchivo { get; set; } = null!;
+
+        public DateTime FechaCreacion { get; set; }
+
+        public virtual Tickets IdTicketNavigation { get; set; } = null!;
+
+        public virtual Usuarios IdUsuarioNavigation { get; set; } = null!;
     }
 }
